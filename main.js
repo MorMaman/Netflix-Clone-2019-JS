16530567baee44cc2a56d92da92b5b28ae58db35
Mor <mor@MacBook-Pro.local>
2022-01-28 22:21:20 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+const tabItems = document.querySelectorAll(".tab-item");
+const tabContent = document.querySelectorAll(".tab-content-item");
+
+function selectItem(e) {
+  removeBorder();
+  removeShow();
+  this.classList.add("tab-border");
+  console.log(this.id);
+  const tabContentItem = document.querySelector(`#${this.id}-content`);
+  tabContentItem.classList.add("show");
+}
+
+function removeBorder() {
+  tabItems.forEach((item) => item.classList.remove("tab-border"));
+}
+
+function removeShow() {
+  tabContent.forEach((item) => item.classList.remove("show"));
+}
+
+tabItems.forEach((item) => item.addEventListener("click", selectItem));
